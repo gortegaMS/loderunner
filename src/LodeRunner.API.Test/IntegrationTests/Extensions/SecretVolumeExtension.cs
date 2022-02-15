11df071f6074c585bf8fa59a04048527478e3755
gortegaMS <gortega@GORTEGA-MSFT2.localdomain>
2022-02-15 15:42:09 -0600

    added secrest dir

--- src/LodeRunner.API.Test/IntegrationTests/Extensions/SecretVolumeExtension.cs
+++ src/LodeRunner.API.Test/IntegrationTests/Extensions/SecretVolumeExtension.cs
@@ -17,7 +17,7 @@ namespace LodeRunner.API.Test.IntegrationTests.Extensions
             if (System.OperatingSystem.IsLinux())
             {
                 //return $"../../../../../temp/{volume}";
-                return $"home/secrets";
+                return $"/home/secrets";
             }
             else
             {
@@ -32,7 +32,7 @@ namespace LodeRunner.API.Test.IntegrationTests.Extensions
             if (System.OperatingSystem.IsLinux())
             {
                 //return $"../../../temp/{volume}";
-                return $"home/secrets";
+                return $"/home/secrets";
             }
             else
             {
